ec548b31f99a222a1d04f4231aae500cd21bcf29
Juan Eduardo Ramirez <the_one_king_90@hotmail.com>
2023-05-09 00:02:49 -0300

    corregi slider js, corregí ruta de js en index y body en css

--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,24 @@
 (function(){
-    const sliders=[...querySelectorAll(".slider_body")];
-    const arrowNext=document.querySelector("#next");
-    const arrowBefore=document.querySelector("#before");
+    const sliders=[...document.querySelectorAll(".slider_body")];
+    const arrowNext = document.querySelector("#next");
+    const arrowBefore = document.querySelector("#before");
     let value;
 
-    arrowNext.addEventListener("click", ()=>changePosition(1) );
-    arrowBefore.addEventListener("click", ()=>changePosition(-1) );
+    arrowNext.addEventListener("click", ()=>changePosition(1));
+    arrowBefore.addEventListener("click", ()=>changePosition(-1));
 
     function changePosition(change){
         const currentElement=Number(document.querySelector(".slider_body--show").dataset.id);
 
-        value=currentElement;
-        value+=change;
+        value= currentElement;
+        value+= change;
 
-        console.log(sliders.lenght)
-        if(value===0 || value==sliders.lenght+1){
-            value= value === 0 ? sliders.lenght : 1;
+        console.log(sliders.length)
+        if(value === 0 || value== sliders.length+1){
+            value= value === 0 ? sliders.length : 1;
         }
 
-        sliders[currentElement-1].classList.toggle("slider body--show");
-        sliders[value-1].classList.toggle("slider body--show");
+        sliders[currentElement-1].classList.toggle("slider_body--show");
+        sliders[value-1].classList.toggle("slider_body--show");
     }
 })()
